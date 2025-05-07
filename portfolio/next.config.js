9190262489ecf339bef0a0e23af36107e89dc91a
Kumareshan S <kumarex30@gmail.com>
2025-05-07 20:32:35 +1000

    Static page commits

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
   webpack(config) {
     // Alias Drei’s Stats import if used elsewhere
     config.resolve.alias['three/examples/js/libs/stats.min'] =
